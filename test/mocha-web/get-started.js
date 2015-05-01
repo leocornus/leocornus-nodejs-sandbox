57a466bf780a9543a0148c12827b2c4d72d508ff
Sean Chen <sean.chen@leocorn.com>
2015-05-01 09:26:52 -0400

    play around with the JavaScript testing...

--- test/mocha-web/get-started.js
+++ test/mocha-web/get-started.js
@@ -1,19 +1,30 @@
 // JavaScript testing using mocha.
 
+// load the chai assertion lib.
 chai.should();
 
 describe('JavaScript run on browser', function() {
-  describe('testing array method indexOf()', function() {
+  describe('testing array', function() {
     var numberArray = [1,2,3];
 
-    it('array index starts from 0', function() {
+    it('length: we have an array with 3 numbers.', function() {
+      numberArray.length.should.equal(3);
+    });
+
+    it('indexOf(): array index starts from 0', function() {
       numberArray.indexOf(1).should.equal(0);
       numberArray.indexOf(2).should.equal(1);
     });
 
-    it('return -1 if the value is not present', function() {
+    it('[0]: will return the first number', function() {
+      numberArray[0].should.equal(1);
+      numberArray[2].should.equal(3);
+    });
+
+    it('indexOf(): return -1 if the value is not present', 
+       function() {
       numberArray.indexOf(0).should.equal(-1);
       numberArray.indexOf(4).should.equal(-1);
     });
-  })
+  });
 })
