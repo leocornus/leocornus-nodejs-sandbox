2ff16eabd5fa01faa4f6900c3a51f130066b8cf1
Sean Chen <sean.chen@leocorn.com>
2015-10-01 09:11:38 -0400

    using gulp to run Jasmine test cases.

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,28 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+// jamine runner.
+var jasmine = require('gulp-jasmine');
 
 gulp.task('default', function() {
   // place code for your default task here
+  console.log('Hello Gulp World!');
 });
 
 // mocha test runner.
 gulp.task('mocha', function() {
 
+  // the src method will locate the source file.
   return gulp.src(['test/mocha/*.js'], {read: false})
     .pipe(mocha({
       reporter: 'spec'
     }));
 
+});
+
+// run jasmine test cases.
+gulp.task('jasmine', function() {
+
+  return gulp.src('test/jasmine/*.js').pipe(jasmine());
 });
 
 // testing yahoo stream.
@@ -22,8 +32,9 @@ gulp.task('yahoo', function() {
     {frequency: 5000}
   );
 
-  stocks.watch('rht');
-  stocks.watch('mcp');
+  stocks.watch('RHT');
+  stocks.watch('FIT');
+  stocks.watch('TSLA');
 
   stocks.on('data', function(stock) {
     console.log('%s: %d', stock.symbol, stock.bid);
