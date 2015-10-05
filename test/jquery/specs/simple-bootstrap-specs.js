56e9ffab341a7bd61f36d0bc38863ddd9c06b476
Sean Chen <sean.chen@leocorn.com>
2015-10-05 14:36:54 -0400

    the simple test case for bootstrap.

---
+++ test/jquery/specs/simple-bootstrap-specs.js
@@ -0,0 +1,33 @@
+describe('Testing Bootstrap Basic', function() {
+
+    var $container;
+
+    beforeEach(function() {
+
+        // load the HTML fixture.
+        jasmine.getFixtures().fixturesPath = 
+            'base/test/jquery/fixtures';
+        loadFixtures('simple-boot.html');
+        // load the container.
+        $container = $('.container');
+    });
+
+    it('testing find method', function() {
+        // find by id
+        var id = $container.find('.nav .active').attr('id');
+        expect(id).toBe('home');
+    });
+
+    it('testing data-toggle', function() {
+        // find id to click.
+        var $about = $container.find('.nav #about');
+        var $home = $container.find('.nav #home');
+        var $aboutHerf = $container.find('.nav #about-a');
+        expect($home.hasClass('active')).toBe(true);
+        expect($about.hasClass('active')).toBe(false);
+        // 
+        $aboutHerf.click();
+        expect($home.hasClass('active')).toBe(false);
+        expect($about.hasClass('active')).toBe(true);
+    });
+});
