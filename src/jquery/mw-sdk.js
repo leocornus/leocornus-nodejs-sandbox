3efc65f52ab07a714cba74338e471aa3ed363cb3
Sean Chen <sean.chen@leocorn.com>
2015-10-19 14:53:00 -0400

    create the method to get a wiki page and process the content.

--- src/jquery/mw-sdk.js
+++ src/jquery/mw-sdk.js
@@ -34,8 +34,11 @@
      *     };
      */
     function MediaWikiClient(siteOptions) {
+
         this.siteOptions = jQuery.extend({}, defaultSite, 
                                          siteOptions);
+        // the raw data from wiki site, in json format.
+        this.rawData = null;
     }
     
     /**
@@ -43,21 +46,22 @@
      */
     jQuery.extend(MediaWikiClient.prototype, {
 
-        // query method. using jQuery.get
-        query: function(title) {
+        // general method for get method.
+        apiGet: function(action, callback) {
             // tracking the original this.
             var self = this;
 
-            // get ready the data send to wiki api.php.
-            var action = {
-                'format' : 'json',
-                'action' : 'query',
-                'titles' : title,
-                'prop' : 'categories'
-            };
-            jQuery.get(self.getApiUrl(), action, function(data) {
-                self.log(data);
-            });
+            jQuery.get(self.getApiUrl(), action)
+              .done(function(data) {
+                  //self.log(data);
+                  //self.setRawData(data);
+                  //self.rawData = data;
+                  callback(null, data);
+              })
+              // handle erro.
+              .fail(function(data) {
+                  callback('Error', data);
+              });
         },
 
         // return the full url to api.php.
@@ -67,6 +71,75 @@
                       this.siteOptions.baseUrl + "/" + 
                       this.siteOptions.apiPath;
             return url;
+        },
+
+        // get an article
+        getArticle: function(title, callback) {
+
+            // save the original this.
+            var self = this;
+
+            // get ready the data send to wiki api.php.
+            var action = {
+                'format' : 'json',
+                'action' : 'parse',
+                'page' : title,
+                'prop' : 'text'
+            };
+
+            // call wiki api
+            this.apiGet(action, function(err, data) {
+
+                if(err) {
+                    callback(err);
+                    return;
+                }
+
+                // TODO the page content.
+                var content = data.parse.text['*'];
+                // process the content and to match bootstrap.
+                content = self.processArticleContent(content);
+                callback(null, content);
+            });
+        },
+
+        // process the arcicle content to toc and content
+        processArticleContent: function(content) {
+
+            // parse the content html to a jQuery object.
+            var $content = jQuery('<div>').html(content);
+            // find the TOC div.
+            var $toc = $content.find('div#toc');
+            //alert(tocHtml);
+            // convert the toc to bootstrap scroll spy.
+            var $nav = jQuery('<nav class="bs-docs-sidebar affix">').
+                       html($toc.html());
+            $nav.find('ul').addClass('nav');
+            var $toc = jQuery('<div>').html($nav);
+
+            // remove TOC from content.
+            $content.find('div#toc').replaceWith('');
+            // remove the edit seciton for each heading.
+            $content.find('span.mw-editsection').replaceWith('');
+            var ret = {'toc' : $toc.html(), 
+                       'content' : $content.html()};
+            return ret; 
+        },
+
+        // get the raw data.
+        getRawData: function() {
+
+            return this.rawData;
+        },
+
+        // set the raw data.
+        setRawData: function(data) {
+
+           this.rawData = data;
+        },
+
+        getPageTitle: function() {
+            
         },
 
         // utility method to log message.
