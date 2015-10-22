013291980f2ed3eada7813b92ed29e653b51aca1
Sean Chen <sean.chen@leocorn.com>
2015-10-22 08:27:23 -0400

    organize the spec and add test case for unshift function.

--- test/jquery/specs/basic-javascript-spec.js
+++ test/jquery/specs/basic-javascript-spec.js
@@ -29,19 +29,48 @@ describe('Basic JavaScript Testing Spec', function() {
         expect(result.length).toBe(2);
     });
 
-    it('Testing the functions shift and unshift', function() {
+    describe('Testing array prototype functions', function() {
 
         /**
-         * the shift and unshift are prototype functions for
-         * array.
-         * undefined will be return for an empty array.
+         * The popular array prototype functions including:
+         *  - push
+         *  - pop
+         *  - shift
+         *  - unshift
          */
-        var testArray = ['a', 'b', 'c', 'd']
-        expect(testArray.length).toBe(4);
-        // shift will remove the first value of an array
-        // remove the value from array consecutively.
-        var shifted = testArray.shift();
-        expect(shifted).toBe('a')
-        expect(testArray.length).toBe(3);
+
+        // define the testing array here, so it will be available
+        // for all functions.
+        var testArray;
+
+        // get ready some array for testing.
+        beforeEach(function() {
+
+            testArray = ['a', 'b', 'c', 'd'];
+        });
+
+        it('Testing the functions shift and unshift', function() {
+
+            /**
+             * the shift and unshift are prototype functions for
+             * array.
+             * undefined will be return for an empty array.
+             */
+
+            // we have 4 items in the testArray which is set up in
+            // the beforeEach funtion.
+            expect(testArray.length).toBe(4);
+            // shift will remove the first value of an array
+            // remove the value from array consecutively.
+            var shifted = testArray.shift();
+            expect(shifted).toBe('a');
+            expect(testArray.length).toBe(3);
+
+            // now we unshift the same array.
+            var newSize = testArray.unshift('a');
+            // unshift wil return the length of unshifted array.
+            expect(newSize).toBe(4);
+            expect(testArray.length).toBe(4);
+        });
     });
 });
