5430a5741645a63154e144fe6d167354a90a7f9f
Sean Chen <sean.chen@leocorn.com>
2015-11-13 15:21:05 -0500

    fix the typo in file name.

--- test/protractor/simple-one-spec.js
+++ test/protractor/simple-one-spec.js
@@ -3,7 +3,7 @@ describe('Simple HTML Page', function() {
     beforeEach(function() {
         // load the home page.
         // relative to the baseUrl in protractor.conf.js.
-        browser.get('demo/protractor/simplet-one.html');
+        browser.get('demo/protractor/simple-one.html');
     });
 
     it('has correct page title', function() {
