ee6c71deb559b9e8473dc5c7e262ee1f15657bc5
Sean Chen <sean.chen@leocorn.com>
2015-11-20 08:44:30 -0500

    a simple case to do end to end test for JavaScript

--- test/protractor/simple-one-spec.js
+++ test/protractor/simple-one-spec.js
@@ -16,4 +16,17 @@ describe('Simple HTML Page', function() {
         expect(header.getInnerHtml()).toEqual('<h1>Hello Web Server</h1>');
         expect(header.getText()).toEqual('Hello Web Server');
     });
+
+    describe('simple JavaScript test', function() {
+
+        it('click button to change sum', function() {
+
+            var plusButton = element(by.id('plus'));
+            var sumDiv = element(by.id('sum'));
+
+            expect(sumDiv.getInnerHtml()).toEqual('nothing');
+            plusButton.click();
+            expect(sumDiv.getInnerHtml()).toEqual('Hello Click');
+        });
+    });
 });
