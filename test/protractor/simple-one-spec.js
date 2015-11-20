b025ab05f72e0d91e9e6122ad4bd66a81b9f3df9
Sean Chen <sean.chen@leocorn.com>
2015-11-19 23:16:10 -0500

    more simple test cases to learn protractor

--- test/protractor/simple-one-spec.js
+++ test/protractor/simple-one-spec.js
@@ -10,4 +10,10 @@ describe('Simple HTML Page', function() {
 
         expect(browser.getTitle()).toEqual('Testing Page');
     });
+
+    it('has page header class', function() {
+        var header = element(by.css('.page-header'));
+        expect(header.getInnerHtml()).toEqual('<h1>Hello Web Server</h1>');
+        expect(header.getText()).toEqual('Hello Web Server');
+    });
 });
