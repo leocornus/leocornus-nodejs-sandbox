ecd8b0abfd7c9d01119a2dea641a7591dd10518d
Sean Chen <sean.chen@leocorn.com>
2015-12-07 10:43:23 -0500

    update the testing search endpoint with more result fields.

--- test/express/search.js
+++ test/express/search.js
@@ -12,11 +12,22 @@ var search = function(req, res) {
         "Item Four"
     ];
 
+    var longestDesc = 'one one one one one one one one, ' +
+        'one one one one one one one one, ' + 
+        'one one one one one one one one, ' + 
+        'one one one one one one one one';
+
     // hard code some result.
     var objectResult = [
-        {title:'The Title', url:'http://one.com'},
-        {title:'The Two', url:'http://two.com'},
-        {title:'The Three', url:'http://three.com'}
+        {title:'The Title', url:'http://one.com',
+         description: longestDesc
+        },
+        {title:'The Two', url:'http://two.com',
+         description: 'one one one one two two two two two'
+        },
+        {title:'The Three', url:'http://three.com',
+         description: 'three three three'
+        }
     ];
 
     res.send(objectResult);
