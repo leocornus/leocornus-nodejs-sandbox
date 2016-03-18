975c349bec0ed28df67f21c2ba0fe54116e27d66
Sean Chen <sean.chen@leocorn.com>
2016-03-18 18:04:50 -0400

    let's start the end to end testing for like button from beginning

--- test/protractor/like-dislike-spec.js
+++ test/protractor/like-dislike-spec.js
@@ -10,113 +10,113 @@ describe('test like dislike demo page', function () {
         browser.get('demo/jquery/like-dislike-demo.html');
 
         // set elements for post vote box and comment vote box
-        post_vote_box = element(by.className('article-votes'));
+        postVoteBox= element(by.className('article-buttons'));
         comment_vote_box = element(by.className('vb'));
     });
 
     it('should load the Vote Box for the post', function () {
 
-        expect(comment_vote_box.getInnerHtml()).not.toBe('');
+        expect(postVoteBox.getInnerHtml()).not.toBe('');
         // pause 1 second
         browser.sleep(1000);
     });
 
-    it('should load the Vote Box for the comments', function () {
+    //it('should load the Vote Box for the comments', function () {
 
-        expect(comment_vote_box.getInnerHtml()).not.toBe('');
-        browser.sleep(1000);
-    });
+    //    expect(comment_vote_box.getInnerHtml()).not.toBe('');
+    //    browser.sleep(1000);
+    //});
 
-    it('should load images for the post vote box', function () {
+    //it('should load images for the post vote box', function () {
 
-        var arr = ['ballot', 'voteup', 'votedown'];
-        for (i = 0; i < 3; i++) {
-            post_vote_box = element(by.id(arr[i]));
-            expect(post_vote_box.getAttribute('src')).not.toBe('');
-        }
+    //    var arr = ['ballot', 'voteup', 'votedown'];
+    //    for (i = 0; i < 3; i++) {
+    //        post_vote_box = element(by.id(arr[i]));
+    //        expect(post_vote_box.getAttribute('src')).not.toBe('');
+    //    }
 
-        browser.sleep(1000);
-    });
+    //    browser.sleep(1000);
+    //});
 
-    it('should load images for the comment vote box', function () {
+    //it('should load images for the comment vote box', function () {
 
-        expect(element(by.css('.comment_approve')).
-               getAttribute('src')).not.toBe('');
-        expect(element(by.css('.comment_disapprove')).
-               getAttribute('src')).not.toBe('');
+    //    expect(element(by.css('.comment_approve')).
+    //           getAttribute('src')).not.toBe('');
+    //    expect(element(by.css('.comment_disapprove')).
+    //           getAttribute('src')).not.toBe('');
 
-        browser.sleep(1000);
-    });
+    //    browser.sleep(1000);
+    //});
 
-    /**
-     * Test the like button.
-     */
-    it('should change voteup divs image and increase vote by 1 ' +
-       'on click for the post vote box', function () {
+    ///**
+    // * Test the like button.
+    // */
+    //it('should change voteup divs image and increase vote by 1 ' +
+    //   'on click for the post vote box', function () {
 
-        post_vote_box = element(by.id('voteup'));
-        post_votes = element(by.id('count-text'));
-        post_votes.getText().then(function (old_vote) {
+    //    post_vote_box = element(by.id('voteup'));
+    //    post_votes = element(by.id('count-text'));
+    //    post_votes.getText().then(function (old_vote) {
 
-            post_vote_box.click();
-            post_votes.getText().then(function(new_vote) {
-                expect(parseInt(new_vote)).
-                    toEqual(parseInt(old_vote) + 1);
-                expect(post_vote_box.getAttribute("src")).
-                    toContain('approve-done.png');
-            });
-        }); 
+    //        post_vote_box.click();
+    //        post_votes.getText().then(function(new_vote) {
+    //            expect(parseInt(new_vote)).
+    //                toEqual(parseInt(old_vote) + 1);
+    //            expect(post_vote_box.getAttribute("src")).
+    //                toContain('approve-done.png');
+    //        });
+    //    }); 
 
-        browser.sleep(2000);
-    });
+    //    browser.sleep(2000);
+    //});
 
-    /**
-     * test the like button on comment.
-     */
-    it('should change voteup divs image and increase vote by 1 ' +
-       'on click for the comment vote box', function () {
+    ///**
+    // * test the like button on comment.
+    // */
+    //it('should change voteup divs image and increase vote by 1 ' +
+    //   'on click for the comment vote box', function () {
 
-        comment_vote_box = element(by.css('.comment_approve'));
-        comment_votes = element(by.id('comment_votes'));
-        comment_votes.getText().then(function (old_vote) {
+    //    comment_vote_box = element(by.css('.comment_approve'));
+    //    comment_votes = element(by.id('comment_votes'));
+    //    comment_votes.getText().then(function (old_vote) {
 
-            comment_vote_box.click();
-            comment_votes.getText().then(function(new_vote) {
-                expect(parseInt(new_vote)).
-                    toEqual(parseInt(old_vote) + 1);
-                expect(comment_vote_box.getAttribute("src")).
-                    toContain('approve-small-done.png');
-            });
-        });
+    //        comment_vote_box.click();
+    //        comment_votes.getText().then(function(new_vote) {
+    //            expect(parseInt(new_vote)).
+    //                toEqual(parseInt(old_vote) + 1);
+    //            expect(comment_vote_box.getAttribute("src")).
+    //                toContain('approve-small-done.png');
+    //        });
+    //    });
 
-        browser.sleep(2000);
-    });
+    //    browser.sleep(2000);
+    //});
 
-    /**
-     * test the dislike button on article
-     */
-    it('should change votedown divs image on click for ' +
-       'the post vote box', function () {
+    ///**
+    // * test the dislike button on article
+    // */
+    //it('should change votedown divs image on click for ' +
+    //   'the post vote box', function () {
 
-        post_vote_box = element(by.id('votedown'));
-        post_vote_box.click();
-        expect(post_vote_box.getAttribute("src")).
-            toContain('disapprove-marked.png');
+    //    post_vote_box = element(by.id('votedown'));
+    //    post_vote_box.click();
+    //    expect(post_vote_box.getAttribute("src")).
+    //        toContain('disapprove-marked.png');
 
-        browser.sleep(2000);
-    });
+    //    browser.sleep(2000);
+    //});
 
-    /**
-     * test the dislike button on comment.
-     */
-    it('should change votedown divs image on click for ' +
-       'the comment vote box', function () {
+    ///**
+    // * test the dislike button on comment.
+    // */
+    //it('should change votedown divs image on click for ' +
+    //   'the comment vote box', function () {
 
-        comment_vote_box = element(by.css('.comment_disapprove'));
-        comment_vote_box.click();
-        expect(comment_vote_box.getAttribute("src")).
-            toContain('disapprove-small-marked.png');
+    //    comment_vote_box = element(by.css('.comment_disapprove'));
+    //    comment_vote_box.click();
+    //    expect(comment_vote_box.getAttribute("src")).
+    //        toContain('disapprove-small-marked.png');
 
-        browser.sleep(2000);
-    });
+    //    browser.sleep(2000);
+    //});
 });
