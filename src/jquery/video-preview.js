379570c31593298e27298830327b84c44d025c36
Sean Chen <sean.chen@leocorn.com>
2016-07-22 11:59:23 -0400

    get ready the mockups for customization form and preview div

--- src/jquery/video-preview.js
+++ src/jquery/video-preview.js
@@ -26,17 +26,78 @@
 
     // use extend method to avoid Plugin prototype confilcts.
     $.extend(Plugin.prototype, {
-        // the initialize function.
+
+        /**
+         * the initialize function.
+         */
         init: function() {
             var self = this;
             var $element = $(this.element);
 
-            // when mouseover, we will show the red back ground.
-            $element.on('mouseover.' + pluginVideoPreview, function() {
+            var previewHtml = self.buildCustomizeForm();
+            $element.html(previewHtml);
+        },
 
-                var $me = $(this);
-                $me.css('background-color', 'red');
-            });
+        /**
+         * build the customization form, using the form-horizontal
+         * class from bootstrap
+         */
+        buildCustomizeForm: function() {
+            var self = this;
+
+            // build the input form groups. 
+            var sourceHtml = self.buildFormGroup('inputSource',
+                'url', 'Source', 'Media Source', 
+                'http://vjs.zencdn.net/v/oceans.mp4');
+            var widthHtml = self.buildFormGroup('inputWidth',
+                'number', 'Width', 'Media Width', 640);
+            var heightHtml = self.buildFormGroup('inputHeight',
+                'url', 'Height', 'Media Height', 364); 
+            var formHtml = 
+              '<div class="col-md-4">' +
+              '  <h2>Customization Form</h2>' +
+              '  <form class="form-horizontal">' +
+                  sourceHtml +
+                  widthHtml + heightHtml +
+              '    <div class="form-group">' +
+              '      <div class="col-sm-offset-2 col-sm-10">' +
+              '        <button type="button" id="preview"' +
+              '                class="btn btn-default">' +
+              '        Preview' +
+              '        </button>' +
+              '      </div>' +
+              '    </div>' +
+              '  </form>' + 
+              '</div>' +
+              '<div class="col-md-8">' +
+              '  <h2>Preview HTML 5 player</h2>' +
+              '  <div id="the-video"></div>' +
+              '</div>';
+
+            return formHtml;
+        },
+
+        /**
+         * build a form group, which have the form-group class with 
+         * lable and an input field.
+         */
+        buildFormGroup: function(id, type, label, placeholder, 
+                                 value) {
+
+            var groupHtml = 
+              '<div class="form-group">' +
+              '  <label for="' + id + '"' +
+              '         class="col-sm-2 control-label">' +
+              '  ' + label + '</label>' +
+              '  <div class="col-sm-10">' +
+              '    <input type="' + type + '" class="form-control"' +
+              '           id="' + id + '"' +
+              '           placeholder="' + placeholder + '"' +
+              '           value="'+ value + '">' +
+              '  </div>' +
+              '</div>';
+
+            return groupHtml;
         }
     });
 
