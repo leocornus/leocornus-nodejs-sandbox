768153715182d505a585c5aace3e120fb65d49a7
Sean Chen <sean.chen@leocorn.com>
2016-12-19 10:57:00 -0500

    update to escape the dot, which is a special character.

--- test/js-core/javascript-regexp-spec.js
+++ test/js-core/javascript-regexp-spec.js
@@ -11,7 +11,7 @@ describe('JavaScript RegExp Prototype Testing Specs', function() {
         it('using exec to extract string from the source', function() {
 
             var source = "http://www.media.gov.on.ca/player/5.1.818/player.swf?config=http://www.media.gov.on.ca/5ff96c18fba293a0/fr/config.xml";
-            var pattern = /config=(http.*config.xml)/;
+            var pattern = /config=(http.*config\.xml)/;
             var result = pattern.exec(source);
             expect(result.length).toBe(2);
             expect(result[1]).toMatch('http://www.media.gov.on.ca/5ff96c18fba293a0/fr/config.xml');
