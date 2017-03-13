2a0ffc730aa78e64e9997b1155a2dd9e3578204c
Sean Chen <sean.chen@leocorn.com>
2017-03-13 11:25:10 -0400

    add more test cases for JavaScript regex.

--- test/js-core/javascript-regexp-spec.js
+++ test/js-core/javascript-regexp-spec.js
@@ -16,5 +16,32 @@ describe('JavaScript RegExp Prototype Testing Specs', function() {
             expect(result.length).toBe(2);
             expect(result[1]).toMatch('http://www.media.gov.on.ca/5ff96c18fba293a0/fr/config.xml');
         });
+
+        it('extract the beginning of a URI path', function() {
+
+            // set the pattern.
+            var pattern = /^\/(.*)\//;
+
+            // the case one.
+            var source = "/wp-admin/themes.php";
+            var result = pattern.exec(source);
+
+            expect(result.length).toBe(2);
+            expect(result[1]).toMatch('wp-admin');
+
+            source = "/wiki/Web_accessibility_guide";
+            result = pattern.exec(source);
+            expect(result[1]).toMatch('wiki');
+
+            source ="/?s=abc";
+            result = pattern.exec(source);
+            expect(result).toBe(null);
+            expect(result === null).toBe(true);
+
+            source ="";
+            result = pattern.exec(source);
+            expect(result).toBe(null);
+            expect(result === null).toBe(true);
+        });
     });
 });
