5dde1cddfe3f72c0916bebb856492722945a1eee
Sean Chen <sean.chen@leocorn.com>
2017-09-11 12:03:04 -0400

    try different layout.

--- src/justified-layout/simple-layout.js
+++ src/justified-layout/simple-layout.js
@@ -4,6 +4,9 @@ import justifiedLayout from 'justified-layout';
 
     // get the geometry
     var geometry = justifiedLayout([1.33, 1, 0.65]);
+    console.log(geometry);
 
-    console.log(geometry)
+    // one more.
+    var newGeo = justifiedLayout([0.5, 1.5, 1, 1.8, 0.4, 0.7, 0.9, 1.1, 1.7, 2, 2.1]);
+    console.log(newGeo);
 }())
