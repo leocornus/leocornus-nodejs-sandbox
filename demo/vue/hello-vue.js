1c24134c81c9b8821c2b42cd111b40766fd038ec
Sean Chen <sean.chen@leocorn.com>
2017-10-11 12:05:06 -0400

    the prefix v-bind is for attribute data binding.

--- demo/vue/hello-vue.js
+++ demo/vue/hello-vue.js
@@ -2,9 +2,25 @@
  * the Vue apps from Vue.js introduction.
  */
 
+/**
+ * the very first app, just render data to DOM by using
+ * the straightforward template syntax.
+ */
 var app = new Vue({
   el: '#app',
   data: {
     message: 'Hello Vue.js!'
+  }
+});
+
+/**
+ * binding data to HTML element attriburtes.
+ * the v-bind: prefix made the magic!
+ */
+var app2 = new Vue({
+  // el is like the selector.
+  el: "#app-2",
+  data: {
+    title: 'the current date is ' + new Date().toLocaleString()
   }
 });
