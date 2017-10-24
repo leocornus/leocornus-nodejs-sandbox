dedb855766a61de786a3e6da068fc3018820a02b
Sean Chen <sean.chen@leocorn.com>
2017-10-24 12:26:17 -0400

    playaround to expose Vue.js app directly using express.js

---
+++ test/express/ar.js
@@ -0,0 +1,18 @@
+// the very simple express server.
+
+var express = require('express');
+var serveIndex = require('serve-index');
+var app = express();
+
+//load static files.
+app.use('/', express.static('demo/vue'));
+app.use('/', serveIndex('demo/vue'));
+app.use('/bower_components', express.static('bower_components'));
+// hello world simple get.
+app.get('/hello', function(req, res) {
+    res.send('<h1>Hello Express World</h1>');
+});
+
+// start server.
+var server = app.listen(17500, function() {
+});
