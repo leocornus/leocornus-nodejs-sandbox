23baa8cb70816eda3b344c5f7a01a47052e35eba
Sean Chen <sean.chen@leocorn.com>
2017-12-01 13:36:33 -0500

    get started the search adventure.

---
+++ demo/vue/search.js
@@ -0,0 +1,13 @@
+/**
+ * the the Vue application object for the Wizard app.
+ */
+new Vue({
+    el: "#search-app",
+
+    methods: {
+        simpleSearch() {
+            console.log('I am in...');
+        }
+    }
+});
+
