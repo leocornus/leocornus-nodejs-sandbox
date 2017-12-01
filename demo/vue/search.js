a94a7c2ddb9f461bf13cfb1c70991db33d8d4992
Sean Chen <sean.chen@leocorn.com>
2017-12-01 13:44:18 -0500

    add the place holder for search result list.

--- demo/vue/search.js
+++ demo/vue/search.js
@@ -2,11 +2,26 @@
  * the the Vue application object for the Wizard app.
  */
 new Vue({
+    // HTML element selector.
     el: "#search-app",
+
+    data: {
+      results: [
+        { title: 'title one',
+          url: 'http://example.com/one',
+          desc: 'description one'
+        },
+        { title: 'title two',
+          url: 'http://example.com/two',
+          desc: 'description two'
+        }
+      ]
+    },
 
     methods: {
         simpleSearch() {
             console.log('I am in...');
+
         }
     }
 });
