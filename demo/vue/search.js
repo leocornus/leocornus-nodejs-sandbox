b33beda234b894be5bc27ab3c9df9b411b897ab0
Sean Chen <sean.chen@leocorn.com>
2017-12-01 14:24:49 -0500

    try to using jQuery inside of Vue, NOT success yet!

--- demo/vue/search.js
+++ demo/vue/search.js
@@ -1,28 +1,40 @@
-/**
- * the the Vue application object for the Wizard app.
- */
-new Vue({
-    // HTML element selector.
-    el: "#search-app",
-
-    data: {
-      results: [
-        { title: 'title one',
-          url: 'http://example.com/one',
-          desc: 'description one'
-        },
-        { title: 'title two',
-          url: 'http://example.com/two',
-          desc: 'description two'
-        }
-      ]
-    },
-
-    methods: {
-        simpleSearch() {
-            console.log('I am in...');
-
-        }
-    }
+jQuery(document).ready(function($) {
+  /**
+   * the the Vue application object for the Wizard app.
+   */
+  var app = new Vue({
+      // HTML element selector.
+      el: "#search-app",
+  
+      data: {
+        results: [
+          { title: 'title one',
+            url: 'http://example.com/one',
+            desc: 'description one'
+          },
+          { title: 'title two',
+            url: 'http://example.com/two',
+            desc: 'description two'
+          }
+        ]
+      },
+  
+      methods: {
+          simpleSearch() {
+              console.log('I am in...');
+  
+              $.ajax({
+                url: 'https://dev-attivio.sites.leocorn.com/rest/searchApi/simpleCgi',
+                method: 'GET',
+                dataType: 'json',
+                data: {
+                  query:"*"
+                },
+                success: function(response) {
+                    console.log(response);
+                }
+              });
+          }
+      }
+  });
 });
-
