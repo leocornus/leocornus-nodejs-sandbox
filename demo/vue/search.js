5e36f8a67fedbf61fbf40f9d3de4a74101866b72
Sean Chen <sean.chen@leocorn.com>
2017-12-02 23:36:26 -0500

    the proper way to get fields

--- demo/vue/search.js
+++ demo/vue/search.js
@@ -6,30 +6,48 @@ var app = new Vue({
     el: "#search-app",
 
     data: {
+      query: '*',
+      totalHits: 0,
       results: [
-        { title: 'title one',
+        {fields: { 
+          '.id': '123',
+          title: 'title one',
           url: 'http://example.com/one',
           desc: 'description one'
-        },
-        { title: 'title two',
+        }},
+        {fields: {
+          '.id': 'abc',
+          title: 'title two',
           url: 'http://example.com/two',
           desc: 'description two'
-        }
+        }}
       ]
+    },
+
+    computed: {
+      resultSummary: function() {
+          return "Found " + this.totalHits + " in total";
+      }
     },
 
     methods: {
         simpleSearch() {
+            self = this;
             console.log('I am in...');
 
             axios.get('https://dev-attivio.sites.leocorn.com/rest/searchApi/simpleCgi',
             {
               params: {
-                q:"*"
+                q: this.query,
+                hits:20,
+                offset: 0
               }
             })
             .then(function(response) {
-                console.log(response);
+                console.log(JSON.stringify(response.data.documents[0]));
+                console.log(response.data.documents[0].fields['title']);
+                self.totalHits = response.data.totalHits;
+                self.results = response.data.documents;
             })
             .catch(function(error) {
               console.log(error);
