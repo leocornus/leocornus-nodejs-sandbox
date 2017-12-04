f006bbb0d889f1a0f389bdbd587355d1f53bda21
Sean Chen <sean.chen@leocorn.com>
2017-12-04 13:14:34 -0500

    try the ingestApi

---
+++ demo/vue/query-delete.js
@@ -0,0 +1,64 @@
+/**
+ * the the Vue application object for the Wizard app.
+ */
+var app = new Vue({
+    // HTML element selector.
+    el: "#query-app",
+
+    data: {
+      baseUrl: 'https://dev-attivio.sites.leocorn.com/rest/ingestApi',
+      query: 'id:abc',
+      sessionId: 'abc',
+      totalHits: 0,
+      results: [
+        {fields: { 
+          '.id': '123',
+          title: 'title one',
+          url: 'http://example.com/one',
+          description: 'description one'
+        }},
+        {fields: {
+          '.id': 'abc',
+          title: 'title two',
+          url: 'http://example.com/two',
+          description: 'description two'
+        }}
+      ]
+    },
+
+    computed: {
+      resultSummary: function() {
+          return "Found " + this.totalHits + " in total";
+      }
+    },
+
+    methods: {
+        simpleQuery() {
+            self = this;
+            console.log('I am in...');
+
+            axios.get(self.baseUrl + '/connect')
+            .then(function(response) {
+                console.log(response);
+                self.sessionId = response.data;
+                // try to do delet by query.
+                axios.post(self.baseUrl + '/delete/' + self.sessionId,
+                  {
+                      query: self.query,
+                      queryLanguage: "advanced",
+                      queryInitWorkflow: "queryInit"
+                  }
+                )
+                .then(function(response) {
+                  console.log(response);
+                })
+                .catch(function(error) {
+                  console.log(error);
+                });
+            })
+            .catch(function(error) {
+              console.log(error);
+            });
+        }
+    }
+});
