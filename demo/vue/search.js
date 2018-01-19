0646a2b0f9866fb5324073c43bae85220ec794fd
Sean Chen <sean.chen@leocorn.com>
2018-01-19 10:51:08 -0500

    play around the JSON query request

--- demo/vue/search.js
+++ demo/vue/search.js
@@ -30,27 +30,29 @@ var app = new Vue({
             //    offset: 0
             //  }
             //})
-            axios.post('https://dev-attivio.sites.leocorn.com/rest/searchApi/search',
-            //axios.post('https://dev-acis-attivio.sites.leocorn.com/rest/searchApi/search',
+            //axios.post('https://dev-attivio.sites.leocorn.com/rest/searchApi/search',
+            axios.post('https://dev-acis-attivio.sites.leocorn.com/rest/searchApi/search',
             {
+                workflow: "customsearch",
                 query: this.query,
                 queryLanguage: "advanced",
-                realm: "test",
-                username: "sean.chen@leocorn.com",
+                //realm: "test",
+                //username: "sean.chen@leocorn.com",
                 rows:50,
                 offset: 0,
-                facetFilters: [
-                  { "name" : "facetName", 
-                    "label" : "Bucket Display Value", 
-                    "filter" : "fieldName:FACET(bucketValue)" 
-                  }
-                ],
+                //facetFilters: [
+                //  { "name" : "facetName", 
+                //    "label" : "Bucket Display Value", 
+                //    "filter" : "fieldName:FACET(bucketValue)" 
+                //  }
+                //],
                 sort: ["title:ASC"]
             })
             .then(function(response) {
                 self.totalHits = response.data.totalHits;
                 self.results = response.data.documents;
                 if(self.totalHits > 0) {
+                    console.log('total hits: ' + self.totalHits);
                     console.log(JSON.stringify(response.data.documents[0]));
                     console.log(response.data.documents[0].fields['title']);
                 }
