0a44672644cfdc375a733df83366fdb38b8a62b0
Sean Chen <sean.chen@leocorn.com>
2018-03-08 15:58:14 +0000

    make the base url really a base url.

--- demo/vue/query-delete.js
+++ demo/vue/query-delete.js
@@ -6,7 +6,7 @@ var app = new Vue({
     el: "#query-app",
 
     data: {
-      baseUrl: 'https://dev-acis-attivio.sites.leocorn.com/rest/ingestApi',
+      baseUrl: 'https://dev-acis-attivio.sites.leocorn.com/rest',
       query: 'id:abc',
       actionName: 'getSessionCount',
       payload: '',
@@ -91,7 +91,7 @@ var app = new Vue({
             console.log('I am in...');
             self.messages.push('Geting started');
 
-            axios.get(self.baseUrl + '/connect')
+            axios.get(self.baseUrl + '/ingestApi/connect')
             .then(function(response) {
                 console.log(response);
                 self.sessionId = response.data;
@@ -99,24 +99,24 @@ var app = new Vue({
                 // try to do delet by query.
                 self.messages.push("Try to do delete by query");
                 self.messages.push(inputPayload);
-                axios.post(self.baseUrl + '/' + actionEndPoint + '/' + self.sessionId,
+                axios.post(self.baseUrl + '/ingestApi/' + actionEndPoint + '/' + self.sessionId,
                            inputPayload)
                 .then(function(response) {
                   console.log(response);
                   self.messages.push(response);
                   // check queued docs.
-                  axios.get(self.baseUrl + '/getDocumentsQueued/' + self.sessionId)
+                  axios.get(self.baseUrl + '/ingestApi/getDocumentsQueued/' + self.sessionId)
                   .then (function(response) {
                     self.messages.push(response.data + ' docs queued!');
                     // try to commit.
                     self.messages.push("Try to commit queued docs!");
-                    axios.get(self.baseUrl + '/commit/' + self.sessionId)
+                    axios.get(self.baseUrl + '/ingestApi/commit/' + self.sessionId)
                     .then(function(response) {
                       console.log('commit --->');
                       //console.log(response);
                       self.messages.push(response);
                       // disconnect.
-                      axios.get(self.baseUrl + '/disconnect/' + self.sessionId)
+                      axios.get(self.baseUrl + '/ingestApidisconnect/' + self.sessionId)
                       .then(function(response) {
                         self.messages.push(response);
                       });
